d2fb8fa5a30139da246106129660e28047fe4c2e
obivanhelsing <obivanhelsing@gmail.com>
2014-06-09 05:15:30 +0300

    testing images

--- src/pattern/MainClass.java
+++ src/pattern/MainClass.java
@@ -13,54 +13,169 @@ import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
 
 import javax.naming.BinaryRefAddr;
 import javax.swing.text.html.Option;
-
+//yapılacaklar:
+//her class ın her  test resmi için sonuçları bi yerde toplamak lazım 
 public class MainClass {
+	Digit[] classes = new Digit[10];
 	
+	static double temp[]= new double[10];
+	public void classify(){
+		
+	}
+	public MainClass() {
+		// TODO Auto-generated constructor stub
+		for (int i = 0; i < classes.length; i++) {
+			classes[i]= new Digit(i);
+		}
+		
+	}
+	public int  max(){
+
+		double max=temp[0];
+		int counter=0;
+		for( int i=1;i<10;i++){
+			
+			if(temp[i]>max){
+				max=temp[i];
+				counter=i;	
+			}
+			
+			//System.out.println(results[i]);
+		}
+		return counter;
+		
+	}
+	public void testZero(){
+		System.out.println("Results for class 0");
+		System.out.println("====== Test results for class 0 test images =====");
+		for(int k=0 ; k<9;k++)	{
+			for(int i=0;i<10;i++){
+				temp[i]= classes[i].discriminant(classes[0].test, k);
+			}
+			System.out.println("Image "+(k+1)+"  classification ==> "+max());
+		}	
+		
+	}
+	public void testOne(){
+		System.out.println("Results for class 1");
+		System.out.println("====== Test results for class 1 test images =====");
+		for(int k=0 ; k<9;k++)	{
+			for(int i=0;i<10;i++){
+				temp[i]= classes[i].discriminant(classes[1].test, k);
+			}
+			System.out.println("Image "+(k+1)+"  classification ==> "+max());
+		}	
+		
+	}
+	public void testTwo(){
+		System.out.println("Results for class 2");
+		System.out.println("====== Test results for class 2 test images =====");
+		for(int k=0 ; k<9;k++)	{
+			for(int i=0;i<10;i++){
+				temp[i]= classes[i].discriminant(classes[2].test, k);
+			}
+			System.out.println("Image "+(k+1)+"  classification ==> "+max());
+		}	
+		
+	}
+	public void testThree(){
+		System.out.println("Results for class 3");
+		System.out.println("====== Test results for class 3 test images =====");
+		for(int k=0 ; k<9;k++)	{
+			for(int i=0;i<10;i++){
+				temp[i]= classes[i].discriminant(classes[3].test, k);
+			}
+			System.out.println("Image "+(k+1)+"  classification ==> "+max());
+		}	
+		
+	}
+	public void testFour(){
+		System.out.println("Results for class 4");
+		System.out.println("====== Test results for class 4 test images =====");
+		for(int k=0 ; k<9;k++)	{
+			for(int i=0;i<10;i++){
+				temp[i]= classes[i].discriminant(classes[4].test, k);
+			}
+			System.out.println("Image "+(k+1)+"  classification ==> "+max());
+		}	
+		
+	}
+	public void testFive(){
+		System.out.println("Results for class 5");
+		System.out.println("====== Test results for class 5 test images =====");
+		for(int k=0 ; k<9;k++)	{
+			for(int i=0;i<10;i++){
+				temp[i]= classes[i].discriminant(classes[5].test, k);
+			}
+			System.out.println("Image "+(k+1)+"  classification ==> "+max());
+		}	
+		
+	}
+	public void testSix(){
+		System.out.println("Results for class 6");
+		System.out.println("====== Test results for class 6 test images =====");
+		for(int k=0 ; k<9;k++)	{
+			for(int i=0;i<10;i++){
+				temp[i]= classes[i].discriminant(classes[6].test, k);
+			}
+			System.out.println("Image "+(k+1)+"  classification ==> "+max());
+		}	
+		
+	}
+	public void testSeven(){
+		System.out.println("Results for class 7");
+		System.out.println("====== Test results for class 7 test images =====");
+		for(int k=0 ; k<9;k++)	{
+			for(int i=0;i<10;i++){
+				temp[i]= classes[i].discriminant(classes[7].test, k);
+			}
+			System.out.println("Image "+(k+1)+"  classification ==> "+max());
+		}	
+		
+	}
+	public void testEight(){
+		System.out.println("Results for class 8");
+		System.out.println("====== Test results for class 8 test images =====");
+		for(int k=0 ; k<9;k++)	{
+			for(int i=0;i<10;i++){
+				temp[i]= classes[i].discriminant(classes[8].test, k);
+			}
+			System.out.println("Image "+(k+1)+"  classification ==> "+max());
+		}	
+		
+	}
+	public void testNine(){
+		System.out.println("Results for class 9");
+		System.out.println("====== Test results for class 9 test images =====");
+		for(int k=0 ; k<9;k++)	{
+			for(int i=0;i<10;i++){
+				temp[i]= classes[i].discriminant(classes[9].test, k);
+			}
+			System.out.println("Image "+(k+1)+"  classification ==> "+max());
+		}	
+		
+	}
 	
-	static InputStream in;
 	
 	public static void main(String[] args){
 		
-			Path f = Paths.get(new File("").getAbsolutePath()+"/src/pattern/data0.htm");
-			
-			try {
-				in = Files.newInputStream(f,new OpenOption[] {StandardOpenOption.READ});
-				byte b[] = new byte[784];
-				in.read(b);
-				for(int i=0;i<28;i++){
-					for(int j=0;j<28;j++){
-						int c = (int)b[i*28+j];
-						
-						 if(c<0)
-		                   {
-		                      c += 256;
-		                      if(c > 128 )
-		                          c = 1; 
-		                      else    
-		                         c = 0; 
-		                       
-		                   }
-		                   else
-		                   {
-		                      if(c> 128 )
-		                         c = 1; 
-		                      else    
-		                          c = 0; 
-		                    
-		                   }
-						 System.out.print(c+" ");
-					}
-					System.out.println();
-	             }
-						
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				System.out.println("New input stream açılmadı dostum:asdasdasdsad ");
-			}
+		MainClass mc = new MainClass();
+		
+		mc.testZero();
+		mc.testOne();
+		mc.testTwo();
+		mc.testThree();
+		mc.testFour();
+		mc.testFive();
+		mc.testSix();
+		mc.testSeven();
+		mc.testEight();
+		mc.testNine();
+		
 
 }
 }
